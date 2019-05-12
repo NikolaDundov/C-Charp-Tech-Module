5c2bc6689763db66169c95ff73aaf596f01841ed
NikolaDundov <40323955+NikolaDundov@users.noreply.github.com>
2019-05-12 22:50:32 +0300

    Add files via upload

---
+++ Associative Arrays/01 Count Real Numbers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _01_Count_Real_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var numbers = Console.ReadLine()
+                .Split()
+                .Select(double.Parse)
+                .ToList();
+
+            SortedDictionary <double, int> counts = new SortedDictionary <double, int>();
+
+            foreach (var number in numbers)
+            {
+                if (counts.ContainsKey(number))
+                {
+                    counts[number]++;
+                }
+                else
+                {
+                    counts.Add(number, 1);
+                }
+            }
+
+            foreach (var number in counts)
+            {
+                Console.WriteLine($"{number.Key} -> {number.Value}");
+            }
+        }
+    }
+}
