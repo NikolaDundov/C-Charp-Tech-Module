df0f69349704a2accca5fa498f5bfc0a2d95e0ca
NikolaDundov <40323955+NikolaDundov@users.noreply.github.com>
2019-05-12 23:01:40 +0300

    Add files via upload

---
+++ Associative Arrays/05 Word Filter/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05_Word_Filter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                Console.ReadLine()
+                .Split()
+                .Where(x => x.Length % 2 == 0)
+                .ToList()
+                .ForEach(x => Console.WriteLine(x));
+
+            //foreach (var item in words)
+            //{
+            //    Console.WriteLine(item);
+            //}
+        }
+    }
+}
