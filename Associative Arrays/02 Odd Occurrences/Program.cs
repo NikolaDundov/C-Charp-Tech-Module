7a254762819556fdf92c2877e4b5a929b0e7d66f
NikolaDundov <40323955+NikolaDundov@users.noreply.github.com>
2019-05-12 22:52:27 +0300

    Add files via upload

---
+++ Associative Arrays/02 Odd Occurrences/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _02_Odd_Occurrences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var words = Console.ReadLine()
+                .Split()
+                .ToList();
+
+            Dictionary<string, int> occurences = new Dictionary<string, int>();
+
+            foreach (var word in words)
+            {
+                string toLower = word.ToLower();
+                if (!occurences.ContainsKey(toLower))
+                {
+                    occurences[toLower] = 0;
+                }
+                occurences[toLower]++;
+            }
+
+            var final = occurences.Where(x => x.Value % 2 != 0);
+
+            foreach (var word in final)
+            {
+                Console.Write(word.Key + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
