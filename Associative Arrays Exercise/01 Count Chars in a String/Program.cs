fad9d52b440bc700155504ccb52ec058c9749e7d
Никола Дундов <40323955+NikolaDundov@users.noreply.github.com>
2019-05-13 22:02:44 +0300

    Add files via upload
    Exercises for Associative arrays in Tech module 01.2019

---
+++ Associative Arrays Exercise/01 Count Chars in a String/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01_Count_Chars_in_a_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Dictionary<char, int> counts = new Dictionary<char, int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char current = input[i];
+                if (current == ' ')
+                {
+                    continue;
+                }
+
+                if (!counts.ContainsKey(current))
+                {
+                    counts[current] = 0;
+                }
+
+                counts[current] += 1;
+
+            }
+
+            foreach (var item in counts)
+            {
+                Console.WriteLine(item.Key + " -> " + item.Value);
+            }
+        }
+    }
+}
