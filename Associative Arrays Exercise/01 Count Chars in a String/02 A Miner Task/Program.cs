ae54da6b9d0030ea7411ebda3c0f7b13e4a97d14
Никола Дундов <40323955+NikolaDundov@users.noreply.github.com>
2019-05-13 22:05:28 +0300

    Add files via upload

---
+++ Associative Arrays Exercise/01 Count Chars in a String/02 A Miner Task/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02_A_Miner_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> result = new Dictionary<string, int>();
+
+            string resource = Console.ReadLine();
+
+            while (resource != "stop")
+            {
+                int quantity = int.Parse(Console.ReadLine());
+
+                if (result.ContainsKey(resource))
+                {
+                    result[resource] += quantity;
+                }
+                else
+                {
+                    result[resource] = quantity;
+                }
+
+                resource = Console.ReadLine();
+            }
+
+            foreach (var item in result)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+
+        }
+    }
+}
